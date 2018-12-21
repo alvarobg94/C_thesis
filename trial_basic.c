280d0a3197dd6f6a620444e338906f60bd6c93d1
alvarobg94 <alvarobg94@hotmail.com>
2018-12-21 21:36:42 +0100

    pic

---
+++ trial_basic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#define PORT 8080
+#define DevAddr 0x48
+/////////////////GLOBAL VARIABLES
+
+///////////////////////////////////////FUNCTIONS
+//////////////////////////////////////////////// MAIN
+int main() {
+fd=  wiringPiI2CSetup (DevAddr);
+data= wiringPiI2CReadReg8 (int fd, int reg) ;
+printf("%d\n",data );
+
+return(0);
+}
