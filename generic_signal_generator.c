f74a631275bdafde928137fe7d9f31e636e3ae0b
alvarobg94 <alvarobg94@hotmail.com>
2019-01-10 16:52:50 +0100

    re

--- generic_signal_generator.c
+++ generic_signal_generator.c
@@ -82,7 +82,7 @@ int main() {
 
     if (k==SIZE_DATA/n_data){
      j++;
-     d_array= realloc(d_array, (SIZE_DATA*j) * sizeof(double));
+     d_array= realloc(d_array, (SIZE_DATA*2*j) * sizeof(double));
      k=0;}
     nanosleep((const struct timespec[]){{0, t_delay}}, NULL);
     //// Calculate next step
